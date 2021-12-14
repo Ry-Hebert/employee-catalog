d4ec3f18369a8dc0bdab7f0599a5badfc9fd57e9
Ryan <ryan@thecpafirm.com>
2021-12-13 23:35:52 -0700

    update to listing element structure and styles

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,12 @@
 
 
 <div class="card">
-<div class="card-image waves-effect waves-block waves-light">
-  <img class="activator" src="images/office.jpg">
-</div>
 <div class="card-content">
   <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
   <p><a href="#">This is a link</a></p>
+</div>
+<div class="card sticky-action">
+    <div class="card-action">   </div>
 </div>
 <div class="card-reveal">
   <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
