bbfb5f8ae8c18a68d9f2215e041311f046746962
Ryan <ryan@thecpafirm.com>
2021-12-15 22:31:42 -0700

    search progress...

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -18,11 +18,43 @@ let renderEmployeeList = async () =>{
     let dbEmployeeList = await getDB()
     console.log('This is the render Employee List func')
     console.log(dbEmployeeList)
+    let checkSearch = document.querySelector('.employee-search-field').value
+
+    if(checkSearch != ''){
+
+        const search = (arr, s) => {
+            console.log('In Search')
+            console.log(arr)
+            console.log(s)
+            
+            const doesContain = (item) =>{
+                let noPicture = {name: item.name, address: item.address, age: item.age, gender: item.gender, interests: item.interests}
+                let getValues = Object.values(noPicture)
+                let check = getValues.toString().toLowerCase()
+                check = check.replace(/[^a-zA-Z0-9]/g, "")
+                console.log(check)
+                console.log(s.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""))
+                console.log(check.includes(s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")))
+                return check.includes(s.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""))
+            }
+            
+            let matches = arr.filter(doesContain)
+    
+            console.log(matches)
+            return matches
+        };
+        
+        let data = search(dbEmployeeList, checkSearch)
+        console.log("Search Return: ")
+        console.log(data)
+        dbEmployeeList = data
+
+    }
 
     let outDest = document.querySelector('.people-out')
 
     outDest.innerHTML = ""
-    outDest.innerHTML = `<ul class="collection people-out-collection">`
+    outDest.innerHTML = `<ul class="collection people-out-collection"></ul>`
 
     let outDest2 = document.querySelector('.people-out-collection')
 
@@ -48,24 +80,27 @@ let renderEmployeeList = async () =>{
 
     // outDest.innerHTML += "</ul>"
 
-    document.querySelectorAll('.itemTog').forEach(item => item.addEventListener('click', () => {
-        let pleaseWork = item.getAttribute('value')
-        let tryThis1 = `#a${pleaseWork}`
-        let crossOutSelector = document.querySelector(tryThis1)
-        crossOutSelector.classList.toggle("completed")
-     }))
+    // document.querySelectorAll('.itemTog').forEach(item => item.addEventListener('click', () => {
+    //     let pleaseWork = item.getAttribute('value')
+    //     let tryThis1 = `#a${pleaseWork}`
+    //     let crossOutSelector = document.querySelector(tryThis1)
+    //     crossOutSelector.classList.toggle("completed")
+    //  }))
 
-     document.querySelectorAll('.itemDelete').forEach(xItem => {
-        xItem.addEventListener('click', () => {
-            let getID = xItem.getAttribute('value')
+    //  document.querySelectorAll('.itemDelete').forEach(xItem => {
+    //     xItem.addEventListener('click', () => {
+    //         let getID = xItem.getAttribute('value')
 
-            deleteFromDB(getID)
+    //         deleteFromDB(getID)
 
-            renderEmployeeList()
-        })
-    }) 
+    //         renderEmployeeList()
+    //     })
+    // }) 
 }
 
 document.querySelector('#initButton').addEventListener('click', () => initEmployeeDB())
 
 document.querySelector('body').addEventListener('load', renderEmployeeList())
+
+let input = document.querySelector('.employee-search-field')
+input.addEventListener('keydown', () => renderEmployeeList())
